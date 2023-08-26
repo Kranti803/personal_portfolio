0f073b8b28d405b8653d805aa33fddae235a0d09
Kranti Kumar Shah <Krantikumar803@gmail.com>
2023-08-26 10:23:02 +0545

    updated services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,19 +4,19 @@ import { BsGlobe } from "react-icons/bs";
 
 const Services = () => {
   return (
-    <section id="services" className="mt-8 px-6 lg:w-[80vw] m-auto ">
+    <section id="services" className="mt-8 px-6  lg:w-[80vw] m-auto ">
       <div className="flex gap-2 items-center">
         <span className="text-xl md:text-3xl">Services</span>
         <p className="h-[1px] bg-[#9d00ff] w-screen"></p>
       </div>
-      <div className="flex flex-col justify-center gap-6 items-center md:flex-row md:justify-evenly mt-6">
-        <div className="border-2 border-[#9d00ff] rounded-md h-48 w-60 sm:w-96  flex flex-col p-8 gap-8">
+      <div className="sm:px-12 flex flex-col justify-center sm:gap-12 gap-6 items-center md:flex-row sm:justify-evenly mt-6">
+        <div className=" border-2 border-[#9d00ff] rounded-md h-48 w-full   flex flex-col p-8 gap-8">
           <BsGlobe size={50} className="text-[#9d00ff]" />
-          <h3 className="text-lg">Web Developement</h3>
+          <h3 className="text-md sm:text-lg">Web Developement</h3>
         </div>
-        <div className="border-2 border-[#9d00ff] rounded-md h-48 w-60 sm:w-96 flex flex-col p-8 gap-8">
+        <div className="border-2 border-[#9d00ff] rounded-md h-48 w-full  flex flex-col p-8 gap-8">
           <AiOutlineMobile size={50} className="text-[#9d00ff]" />
-          <h3 className="text-lg">Hybrid App Developement</h3>
+          <h3 className="text-md sm:text-lg">Hybrid App Developement</h3>
         </div>
       </div>
     </section>
